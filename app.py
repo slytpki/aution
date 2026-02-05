caf2dc7d3e0a6a276cff6f5b5d5bc15f11bfdccb
slytpki <slyi@mail.moj.gov.tw>
2026-02-05 16:03:21 +0800

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+
+# --- 隱藏 Streamlit 預設物件的 CSS ---
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            .stAppDeployButton {display:none;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+# --- 下面才開始寫您的主程式 ---
 st.title("我的法務部士林分署拍賣")
-st.write("這是測試頁面")
+# ...
